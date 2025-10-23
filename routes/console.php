c6d198cd86ccf3b8f9051007fffc25c39468f822
DnvsrDesarrollo1 <dnvsr.desarrollo.1@hotmail.com>
2025-10-23 09:38:52 -0400

    docs(routes): comment test update in console routes
    Add test comment for future reference in console routes file

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Schedule;
 //Schedule::command('app:prune-inactive')->everyFiveMinutes();
 Schedule::command('app:plan-status')->everyFiveMinutes();
 Schedule::command('app:beneficiary-status')->everyFiveMinutes();
+//ACTUALIZACION TEST
